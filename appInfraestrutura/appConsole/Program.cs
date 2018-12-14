a13a8db698515c07c7c4ff78f7739bea6907849d
patrikmuller <patrik.muller@hotmail.com>
2018-12-14 16:05:20 -0200

    Commit appInfraestrutura/appConsole

---
+++ appInfraestrutura/appConsole/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using appInfraestrutura.Database;
+using appInfraestrutura.Model;
+
+namespace appConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new BancoContext())
+            {
+                context.Database.EnsureCreated();
+            }
+
+            Console.WriteLine("Database!");
+
+            Console.ReadKey();
+
+            using (var context = new BancoContext())
+            {
+                var author = new Author
+                {
+                    FirstName = "William",
+                    LastName = "Shakespeare",
+                    Books = new List<Book>
+                    {
+                        new Book { Title = "Hamlet"},
+                        new Book { Title = "Othello" },
+                        new Book { Title = "MacBeth" }
+                    }
+                };
+                context.Add(author);
+                context.SaveChanges();
+            }
+
+            Console.WriteLine("Save!");
+
+            Console.ReadKey();
+
+            using (var context = new BancoContext())
+            {
+                foreach (var book in context.Books)
+                {
+                    Console.WriteLine(book.Title);
+                }
+            }
+
+            Console.WriteLine("Lista!");
+
+            Console.WriteLine("Hello World!");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ appInfraestrutura/appInfraestrutura/Database/BancoContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using appInfraestrutura.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace appInfraestrutura.Database
+{
+    public class BancoContext : DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Author> Authors { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql(@"Server=localhost;Port=5432;Database=core;User Id=login_core;Password=123456;");
+        }
+
+    }
+}
